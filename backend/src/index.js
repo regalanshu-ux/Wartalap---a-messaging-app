2f16c700617dfbfdef15da2ca483878d75db8299
Anshu <regalanshu@gmail.com>
2026-06-06 15:19:24 +0530

    Configure production static serving and add root build scripts for Render deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
-
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 import {connectDB} from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
@@ -12,6 +12,7 @@ import { app, server } from "./lib/socket.js";
 dotenv.config()
 
 const PORT= process.env.PORT
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
@@ -25,6 +26,13 @@ app.use(cors({
 app.use("/api/auth", authRoutes);
 app.use("/api/message",messageRoutes);
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    });
+}
 
 server.listen(PORT, () => {
     console.log("server is running on PORT: "+PORT);
